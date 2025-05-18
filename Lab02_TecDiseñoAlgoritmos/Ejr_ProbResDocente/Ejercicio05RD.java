37da80bca2264a53e594036d8483533c38a7ac82
danielS <bsenciaa@unsa.edu.pe>
2025-05-17 21:14:53 -0500

    culminacion de prueba de los ejerscicios resueltos por el docente del lab 02

--- Lab02_TecDiseñoAlgoritmos/Ejr_ProbResDocente/Ejercicio05RD.java
+++ Lab02_TecDiseñoAlgoritmos/Ejr_ProbResDocente/Ejercicio05RD.java
@@ -7,9 +7,18 @@ Ej. el factorial de 4 es 4 * 3 * 2 * 1 es decir 24. */
 
 public class Ejercicio05RD {
     
-    public factorial(int fact){
-        if(fact>0){
-            int valor = 
-        }
+    int factorial(int fact) {
+        if (fact > 0) {
+            int valor = fact * factorial(fact - 1);
+            return valor;
+        } else
+        return 1;
     }
+
+    public static void main(String[] args) {
+        Ejercicio05RD re = new Ejercicio05RD();
+        int f = re.factorial(4);
+        System.out.println("El factorial de 4 es " + f);
+    }
+
 }
--- Lab02_TecDiseñoAlgoritmos/Ejr_ProbResDocente/Ejercicio06RD.java
+++ Lab02_TecDiseñoAlgoritmos/Ejr_ProbResDocente/Ejercicio06RD.java
@@ -1,5 +1,32 @@
 package Lab02_TecDiseñoAlgoritmos.Ejr_ProbResDocente;
 
+//Implementar un método recursivo para ordenar los elementos de un vector.
+
 public class Ejercicio06RD {
+    static int[] vec = { 312, 614, 88, 22, 54 };
     
+    void ordenar(int[] v, int cant) {
+        if (cant > 1) {
+            for (int f = 0; f < cant - 1; f++)
+                if (v[f] > v[f + 1]) {
+                    int aux = v[f];
+                    v[f] = v[f + 1];
+                    v[f + 1] = aux;
+                }
+            ordenar(v, cant - 1);
+        }
+    }
+
+    void imprimir() {
+        for (int f = 0; f < vec.length; f++)
+            System.out.print(vec[f] + " ");
+            System.out.println("\n");
+    }
+    public static void main(String[] ar) {
+        Ejercicio06RD r = new Ejercicio06RD();
+        r.imprimir();
+        r.ordenar(vec, vec.length);
+        r.imprimir();
+    }
+
 }
